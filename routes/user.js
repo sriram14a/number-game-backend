0636913a7d0493643cec2195c6f6bd0a6298cc86
sriram14a <sriramjs07@gmail.com>
2023-05-15 17:22:46 +0530

    add

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,5 @@
 import express from "express";
-import {
-  createUser,getUser,updateUser,getWinner
-} from "../controller.js";
+import { createUser, getUser, updateUser, getWinner } from "../controller.js";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -9,66 +7,52 @@ dotenv.config();
 const router = express.Router();
 
 router.post("/userdetails", async (req, res) => {
+  const { name } = req.body;
+  const randomNumber = Math.floor(1000 + Math.random() * 9000);
 
-  const { name } = req.body; 
-  const randomNumber = Math.floor(1000 + Math.random() * 9000)
- 
-  const arr = randomNumber.toString().split('')
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]=== arr[i+1]){
-      if(arr[i] === "9"){
-      arr[i]=parseInt(arr[i])-1}
-      else{
-      arr[i]=parseInt(arr[i])+1
-    }
+  const arr = randomNumber.toString().split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      if (arr[i] === "9") {
+        arr[i] = parseInt(arr[i]) - 1;
+      } else {
+        arr[i] = parseInt(arr[i]) + 1;
+      }
     }
   }
 
-const oldUser = await getUser(name)
-console.log(oldUser)
+  const oldUser = await getUser(name);
 
-
-if(oldUser){
-  res.send({message:"username already exist try unique gaming name"})
-}
-else{
-   await createUser(name,arr);
-  res.send({ status: "ok" });
-}
-
- 
+  if (oldUser) {
+    res.send({ message: "username already exist try unique gaming name" });
+  } else {
+    await createUser(name, arr);
+    res.send({ status: "ok" });
+  }
 });
 
 router.get("/userdetails/:name", async (req, res) => {
+  const { name } = req.params;
+  const oldUser = await getUser(name);
 
-  const { name } = req.params; 
-const oldUser = await getUser(name)
-console.log(oldUser)
-
-if(oldUser){
-  res.send({data:oldUser})
-}
-
+  if (oldUser) {
+    res.send({ data: oldUser });
+  }
 });
 
 router.post("/userdetails/:name", async (req, res) => {
-const {name}=req.params
-  const { guess } = req.body; 
-const oldUser = await getUser(name)
-console.log(oldUser)
+  const { name } = req.params;
+  const { guess } = req.body;
+  const oldUser = await getUser(name);
 
-if(oldUser){
-  await updateUser(name,guess)
-}
-
+  if (oldUser) {
+    await updateUser(name, guess);
+  }
 });
 
 router.get("/userdetails", async (req, res) => {
-
-  const winner = await getWinner(req)
-res.send({data:winner})
- 
+  const winner = await getWinner(req);
+  res.send({ data: winner });
 });
-
 
 export const usersRouter = router;
